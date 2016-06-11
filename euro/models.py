a0b20947f04e8f26480cb062ab16cc91bf8b4b6a
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2016-06-11 18:30:53 +0200

    change points: ajout difference de but exacte

--- euro/models.py
+++ euro/models.py
@@ -116,6 +116,7 @@ def my_callback(sender, instance, **kwargs):
             pronostic.points += 1
         if pronostic.score2 == instance.score2:
             pronostic.points += 1
+        if pronostic.score1 - pronostic.score2 == instance.score1 - instance.score2:
         if pronostic.winner == instance.winner:
             pronostic.points += 1
         pronostic.save()    
