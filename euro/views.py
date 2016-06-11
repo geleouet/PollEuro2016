1e30ca72f753cb733db35483f3ec02ae33c5ee3a
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2016-06-11 13:19:22 +0200

    order by in classement

--- euro/views.py
+++ euro/views.py
@@ -260,7 +260,7 @@ def classement_teams(request):
         user = None
         self_user = None
 
-    teams = Team.objects.prefetch_related('member_set__pronostic_set').annotate(sc=Sum('member__pronostic__points')).order_by('sc').all()
+    teams = Team.objects.prefetch_related('member_set__pronostic_set').annotate(sc=Sum('member__pronostic__points')).order_by('-sc').all()
     
     context = {
         'teams': teams,
