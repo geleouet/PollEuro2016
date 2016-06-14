3efe3523946e9a0d024562f750172ce8ade19a78
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2016-06-14 09:37:26 +0200

    up

--- euro/models.py
+++ euro/models.py
@@ -51,7 +51,7 @@ class Team(models.Model):
     def score(self):
         if self.sc and self.nb:
             if self.nb>1:
-                return (4*(self.sc-self.minp)/(self.nb-1) + self.maxp)/5
+                return (4*((self.sc-self.minp)/(self.nb-1)) + self.maxp)/5
             else :
                 return self.sc
         return 0
